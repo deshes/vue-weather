0534ebfb7e9b78ad2edecb57bf067464aeb406aa
deshes <deshes@qq.com>
2017-01-08 19:35:19 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import App from './App'
+import Vueresource from 'vue-resource'
+Vue.use(ElementUI)
+Vue.use(Vueresource)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
