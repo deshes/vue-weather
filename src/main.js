03e66b6ec36f775f4bb9dfe5305d84d6e06fd907
jxncdq <jxncdq@gmail.com>
2019-06-30 23:32:06 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import Vueresource from 'vue-resource'
 Vue.use(ElementUI)
